a18df3e3696d13bbf169a43842dcd0fd6fa37887
Florian Häcker <haecker.florian@gmail.com>
2024-10-27 16:08:46 +0100

    adjustments to keybinds

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -8,6 +8,11 @@ local mapKey = function(opts)
             modes
         }
     end
+
+    if(opts.silent ~= false) then
+        opts.silent = true
+    end
+
     for _, mode in ipairs(modes) do
         -- vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
         vim.keymap.set(
@@ -16,6 +21,7 @@ local mapKey = function(opts)
             rhs,
             {
                 desc = opts.desc,
+                silent = opts.silent,
             }
         )
     end
@@ -24,19 +30,21 @@ end
 mapKey{desc="save", "n", "<Leader>w", ":w<CR>"}
 mapKey{desc="save all", "n", "<Leader>wa", ":wa<CR>"}
 mapKey{desc="quit", "n", "<Leader>q", ":q<CR>"}
-mapKey{desc="quit all", "n", "<Leader>qa", ":wa<CR>:qa<CR>"}
+mapKey{desc="quit all", "n", "<Leader>qa", ":qa<CR>"}
 
-mapKey{desc="fous nvim-tree", "n", "<Leader>e", ":NvimTreeFocus<CR>"}
+-- NvimTree
+mapKey{desc="focus nvim-tree", "n", "<Leader>e", ":NvimTreeFocus<CR>"}
+mapKey{desc="find file nvim-tree", "n", "<Leader>f", ":NvimTreeFindFile<CR>"}
 
 -- Insert Mode
 mapKey{desc="insert line below", {"i","n"}, "<C-k>", "<Esc>O"}
 mapKey{desc="insert line above", {"i","n"}, "<C-j>", "<Esc>o"}
 
 -- insert lines above/below cursor
-mapKey{desc="move current line up", "n", "<A-k>", ":move -2<CR>"}
-mapKey{desc="move current line down", "n", "<A-j>", ":move +1<CR>"}
-mapKey{desc="move current line up", "i", "<A-k>", "<Esc>:move -2<CR>li"}
-mapKey{desc="move current line down", "i", "<A-j>", "<Esc>:move +1<CR>li"}
+-- mapKey{desc="move current line up", "n", "<A-k>", ":move -2<CR>"}
+-- mapKey{desc="move current line down", "n", "<A-j>", ":move +1<CR>"}
+-- mapKey{desc="move current line up", "i", "<A-k>", "<Esc>:move -2<CR>li"}
+-- mapKey{desc="move current line down", "i", "<A-j>", "<Esc>:move +1<CR>li"}
 
 -- move inside wrapped lines
 mapKey{desc="", {"n","v"}, "j", "gj"}
@@ -44,10 +52,19 @@ mapKey{desc="", {"n","v"}, "k", "gk"}
 mapKey{desc="", {"n","v"}, "gj", "j"}
 mapKey{desc="", {"n","v"}, "gk", "k"}
 
+-- quickly exit insert mode
+mapKey{desc="", {"i"}, "jk", "<Esc>"}
+
 -- clear line
 mapKey{desc="Clear current line", {"n"}, "<Leader>c", "cc<Esc>"}
 
 -- Telescope
 mapKey{"n", "<Leader>sf", ":Telescope find_files<CR>"}
 mapKey{"n", "<Leader>sg", ":Telescope live_grep<CR>"}
-mapKey{"n", "<Leader><Leader>", ":Telescope buffers<CR>"}
+mapKey{"n", "<Leader><Leader>", function()
+    require("telescope.builtin").buffers({ sort_lastused = true, ignore_current_buffer = true })
+end}
+
+-- LSP
+mapKey{desc="Go to declaration", "n", "<Leader>gD", "<cmd>lua vim.lsp.buf.declaration()<CR>"}
+mapKey{desc="Go to definition", "n", "<Leader>gd", "<cmd>lua vim.lsp.buf.definition()<CR>"}
