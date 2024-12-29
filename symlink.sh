0ed9330dd2cf505983140e740286dc3749e8b5c9
Florian Häcker <haecker.florian@gmail.com>
2024-12-29 12:50:39 +0100

    starting ghostty configuration

--- symlink.sh
+++ symlink.sh
@@ -12,8 +12,11 @@ ln -f -s "$PWD/i3/" $HOME/.config/
 # symlink nvim config
 ln -f -s "$PWD/nvim/" $HOME/.config/
 
-# symlink nvim config
+# symlink alacritty config
 ln -f -s "$PWD/alacritty/" $HOME/.config/
+
+# symlink ghostty config
+ln -f -s "$PWD/ghostty/" $HOME/.config/
 
 echo "finished symlinks"
 
