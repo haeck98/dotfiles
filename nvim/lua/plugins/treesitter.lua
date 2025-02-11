952284f29a0cd25de4cb46751159b1a1071a9c60
Florian Häcker <haecker.florian@gmail.com>
2025-02-11 23:28:52 +0100

    FINALLY FIXED THE INJECTIONS FOR ANGULAR YESSSS (of course my fault)

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,3 +1,13 @@
 return {
-	"nvim-treesitter/nvim-treesitter"
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+    config = function()
+	require("nvim-treesitter.configs").setup({
+
+	    highlight = { enable = true },
+	    indent = { enable = true },
+	    incremental_selection = { enable = true },
+	    injections = { enable = true }
+	})
+    end,
 }
