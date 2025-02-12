5f3810d29bf1cbd5e9bfed7e2051a97f63f54544
Florian Häcker <haecker.florian@gmail.com>
2025-02-12 10:16:01 +0100

    added moree mini plugins, added treesitter tokens

---
+++ nvim/lua/plugins/mini.lua
@@ -0,0 +1,68 @@
+
+return {
+  {
+    'echasnovski/mini.ai',
+    event = "VeryLazy",
+    opts = function()
+      local ai = require "mini.ai"
+      return {
+        n_lines = 500,
+        custom_textobjects = {
+          o = ai.gen_spec.treesitter({ -- code block
+            a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+            i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+          }),
+          f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
+          c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
+          t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
+          d = { "%f[%d]%d+" }, -- digits
+          e = { -- Word with case
+            { "%u[%l%d]+%f[^%l%d]", "%f[%S][%l%d]+%f[^%l%d]", "%f[%P][%l%d]+%f[^%l%d]", "^[%l%d]+%f[^%l%d]" },
+            "^().*()$",
+          },
+          -- g = LazyVim.mini.ai_buffer, -- buffer
+          u = ai.gen_spec.function_call(), -- u for "Usage"
+          U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot in function name
+        },
+      }
+    end,
+    config = function(_, opts)
+      local ai = require "mini.ai"
+      ai.setup(opts)
+    end,
+  },
+  {
+    "echasnovski/mini.files",
+    opts = {
+      windows = {
+        preview = true,
+        width_focus = 30,
+        width_preview = 30,
+      },
+      options = {
+        -- Whether to use for editing directories
+        -- Disabled by default in LazyVim because neo-tree is used for that
+        use_as_default_explorer = false,
+      },
+    },
+    keys = {
+      {
+        "<leader>fm",
+        function()
+          require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+        end,
+        desc = "Open mini.files (Directory of Current File)",
+      },
+      {
+        "<leader>fM",
+        function()
+          require("mini.files").open(vim.uv.cwd(), true)
+        end,
+        desc = "Open mini.files (cwd)",
+      },
+    },
+    config = function(_, opts)
+      require("mini.files").setup(opts)
+    end,
+  },
+}
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,36 @@
-return {
-    "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
-    config = function()
-	require("nvim-treesitter.configs").setup({
 
+return {
+    {
+	"nvim-treesitter/nvim-treesitter-textobjects",
+	event = "VeryLazy",
+	enabled = true,
+	config = function()
+	end,
+    },
+    {
+	"nvim-treesitter/nvim-treesitter",
+	event = { "VeryLazy" },
+	lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+	init = function(plugin)
+	    -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+	    -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+	    -- no longer trigger the **nvim-treesitter** module to be loaded in time.
+	    -- Luckily, the only things that those plugins need are the custom queries, which we make available
+	    -- during startup.
+	    require("lazy.core.loader").add_to_rtp(plugin)
+	    require("nvim-treesitter.query_predicates")
+	end,
+	opts = {
 	    highlight = { enable = true },
 	    indent = { enable = true },
 	    incremental_selection = { enable = true },
 	    injections = { enable = true }
-	})
-    end,
+	},
+	config = function(_, opts)
+		--    if type(opts.ensure_installed) == "table" then
+		-- opts.ensure_installed = LazyVim.dedup(opts.ensure_installed)
+		--    end
+	    require("nvim-treesitter.configs").setup(opts)
+	end,
+    },
 }
